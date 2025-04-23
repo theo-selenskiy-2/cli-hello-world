b1f96e94feec338f430d02a20da630faeeea77e9
Theo S <fedor.selenskiy@akeneo.com>
2025-04-23 11:02:08 +0100

    feat: add hello-world command

---
+++ src/App/Commands/HelloWorldCommand.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Console\App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+
+class HelloWorldCommand extends Command
+{
+    protected function configure()
+    {
+        $this->setName('hello-world')->setDescription('Prints Hello-World!')->setHelp('Demo of custom commands')->addArgument('username', InputArgument::REQUIRED, 'Pass the username');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln(sprintf('Hello world! %s', $input->getArgument('username')));
+        return Command::SUCCESS;
+    }
+}
